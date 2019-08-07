b23215c070af52590c4500fd39ab4beeeb00c3af
Yi Li <liyixa@cn.ibm.com>
2019-08-07 09:21:47 +0800

    IC-2019.1.1 <liyixa@liyixa-mbp.cn.ibm.com Create baseRefactoring.xml, markdown.xml, ide.general.xml, project.default.xml, Default.xml, code.style.schemes, find.xml, ui.lnf.xml, filetypes.xml, javaeeExternalResources.xml, plugin_ui.xml, colors.scheme.xml, scala_config.xml, _@user_Darcula.icls, path.macros.xml, Copyright.java, scala.xml, Class.java, jdk.table.xml, laf.xml, proxy.settings.xml, keymap.xml, editor.xml, usageView.xml, debugger.xml, updates.xml

---
+++ fileTemplates/includes/Copyright.java
@@ -0,0 +1,12 @@
+/* ***************************************************************** */
+/*                                                                   */
+/* IBM Confidential                                                  */
+/* OCO Source Materials                                              */
+/*                                                                   */
+/* (C) Copyright IBM Corp. ${YEAR}                                      */
+/*                                                                   */
+/* The source code for this program is not published or otherwise    */
+/* divested of its trade secrets, irrespective of what has been      */
+/* deposited with the U.S. Copyright Office.                         */
+/*                                                                   */
+/* ***************************************************************** */
